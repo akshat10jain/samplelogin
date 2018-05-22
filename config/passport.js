198902c66ff427e404122262507b04493db9b61b
Akshat jain <akshat.jain100896@gmail.com>
2018-05-22 08:12:15 +0530

    initial commit

---
+++ config/passport.js
@@ -0,0 +1,125 @@
+var LocalStrategy    = require('passport-local').Strategy;
+
+// load up the user model
+var User       = require('../app/models/user');
+
+module.exports = function(passport) {
+
+    
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+   
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    
+    // LOCAL LOGIN 
+    passport.use('local-login', new LocalStrategy({
+        
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true 
+    },
+    function(req, email, password, done) {
+        if (email)
+            email = email.toLowerCase(); 
+
+        
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                
+                if (err)
+                    return done(err);
+
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+                // all is well, return user
+                else
+                    return done(null, user);
+            });
+        });
+
+    }));
+
+   
+    // LOCAL SIGNUP
+    passport.use('local-signup', new LocalStrategy({
+       
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true 
+    },
+    function(req, email, password, done) {
+        if (email)
+            email = email.toLowerCase(); 
+
+        
+        process.nextTick(function() {
+            // if the user is not already logged in:
+            if (!req.user) {
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                  
+                    if (err)
+                        return done(err);
+
+                    // check to see if theres already a user with that email
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    } else {
+
+                        // create the user
+                        var newUser            = new User();
+
+                        newUser.local.email    = email;
+                        newUser.local.password = newUser.generateHash(password);
+
+                        newUser.save(function(err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null, newUser);
+                        });
+                    }
+
+                });
+            // if the user is logged in but has no local account...
+            } else if ( !req.user.local.email ) {
+                
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                    if (err)
+                        return done(err);
+                    
+                    if (user) {
+                        return done(null, false, req.flash('loginMessage', 'That email is already taken.'));
+                        
+                    } else {
+                        var user = req.user;
+                        user.local.email = email;
+                        user.local.password = user.generateHash(password);
+                        user.save(function (err) {
+                            if (err)
+                                return done(err);
+                            
+                            return done(null,user);
+                        });
+                    }
+                });
+            } else {
+                // user is logged in and already has a local account. 
+                return done(null, req.user);
+            }
+
+        });
+
+    }));
+};
---
+++ start.js
@@ -0,0 +1,41 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+//connection to database
+mongoose.connect('mongodb://akshat:sample@ds129670.mlab.com:29670/sample', { 
+    useMongoClient: true 
+});
+
+
+require('./config/passport')(passport); 
+
+// set up our express application
+app.use(morgan('dev')); 
+app.use(cookieParser());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'hkjshkbxuiwyiuexyiuwoiuxuiwwnyoiywoi', 
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash());
+
+// routes
+require('./app/routes.js')(app, passport); 
+app.listen(port);
+console.log('The magic happens on port ' + port);
